ef2f0b10ebee92b12cc5e398ca02aceffbd4c86e
Anna Nonaka <annanonaka@gmail.com>
2021-07-10 14:03:07 +0900

    models done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
+
+  validates :comment, length: { minimum: 7 }
+  validates :movie_id, presence: true, uniqueness: { scope: :list_id }, allow_blank: false
+  validates :list_id, presence: true, uniqueness: { scope: :movie_id }, allow_blank: false
 end
